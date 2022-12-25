63ee307134e1a40bc35cf0cfee0b39e1a0a51c50
TheShenk <udod_maksim@mail.ru>
2022-12-25 16:41:49 +0300

    feat(multiagent): base callback support

--- multiagent/multi_model_agent.py
+++ multiagent/multi_model_agent.py
@@ -53,6 +53,7 @@ class MultiModelAgent(BaseAgent):
 
         for model in self.models:
             model.start_learning(total_timesteps)
+        callback.on_training_start(locals(), globals())
 
         with tqdm(total=total_timesteps) as pbar:
             while time < total_timesteps:
@@ -61,24 +62,32 @@ class MultiModelAgent(BaseAgent):
                 for model in self.models:
                     model.start_record()
 
+                callback.on_rollout_start()
+
                 while any([model.continue_record() for model in self.models]):
                     sample_actions_results = [model.sample_action() for model in self.models] + \
                                              [s_model.predict(observation, episode_start=done) for s_model in
                                               self.static_models]
                     actions = tuple(map(lambda x: x[0], sample_actions_results))
                     total_action = self.actions_combiner(actions)
-                    time += self.env.num_envs
-                    pbar.update(self.env.num_envs)
 
                     next_observation, reward, done, info = self.env.step(total_action)
                     total_reward += reward
                     current_step_reward += reward
+
+                    time += self.env.num_envs
+                    pbar.update(self.env.num_envs)
+                    callback.on_step()
 
                     for model, action, sample_actions_result in zip(self.models, actions, sample_actions_results):
                         model.record(observation, action, next_observation, reward, done, info, sample_actions_result)
 
                     observation = next_observation
 
+                callback.on_rollout_end()
+
                 for model in self.models:
                     model.end_record()
                     model.train()
+
+        callback.on_training_end()
