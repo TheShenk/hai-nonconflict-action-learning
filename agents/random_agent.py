3dd257550c91f67234f08c7b6d1d23dfdfcf4b68
TheShenk <udod_maksim@mail.ru>
2023-01-12 11:52:06 +0300

    fix(experiments): use right signature of random agent

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -2,14 +2,14 @@ from typing import Optional, Tuple
 
 import gym
 import numpy as np
+from stable_baselines3.common.type_aliases import GymEnv
 
 from agents.base_agent import BaseAgent
 
 
 class RandomAgent(BaseAgent):
-    def __init__(self, env: gym.Env, action_space=None):
+    def __init__(self, env: GymEnv, action_space=None):
         super().__init__(env)
-        self.env = env
         self.action_space = action_space
 
     def _predict(self,
