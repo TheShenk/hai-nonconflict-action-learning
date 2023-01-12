09798d46460476acea846938d1988f4084049fe9
TheShenk <udod_maksim@mail.ru>
2023-01-12 11:48:58 +0300

    feat(multiagent): add support for static models in two side model

--- multiagent/two_side_model_agent.py
+++ multiagent/two_side_model_agent.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple
 import numpy as np
 from stable_baselines3.common.vec_env import VecEnv, DummyVecEnv
 
+from agents.base_agent import BaseAgent
 from multiagent.action_combiners import BOX_COMBINER, NON_VEC_COMBINER
 from multiagent.multi_agent_proxy import MultiAgentProxy
 from multiagent.multi_model_agent import MultiModelAgent
@@ -14,11 +15,17 @@ class TwoSideModelAgent(MultiModelAgent):
                  env: VecEnv,
                  left_models: List[MultiAgentProxy],
                  right_models: List[MultiAgentProxy],
+                 left_static_models: Optional[List[BaseAgent]] = None,
+                 right_static_models: Optional[List[BaseAgent]] = None,
                  nvec_actions_combiner=NON_VEC_COMBINER,
                  vec_actions_combiner=BOX_COMBINER):
 
+        self.left_static_models = left_static_models if left_static_models is not None else []
+        self.right_static_models = right_static_models if right_static_models is not None else []
+
         models = left_models + right_models
-        super().__init__(env, models,
+        static_models = self.left_static_models + self.right_static_models
+        super().__init__(env, models, static_models,
                          nvec_actions_combiner=nvec_actions_combiner,
                          vec_actions_combiner=vec_actions_combiner)
 
@@ -38,16 +45,28 @@ class TwoSideModelAgent(MultiModelAgent):
         return self.nvec_actions_combiner(actions)
 
 
+    def get_side_total_action(self, side_models, side_static_models, observation, done):
+
+        models_count = len(side_models)
+        static_models_count = len(side_static_models)
+
+        sample_actions_result = [model.sample_action() for model in side_models]
+        for s_model_index in range(static_models_count):
+            model_observation = observation[models_count + s_model_index]
+            sample_actions_result.append(self.static_models[s_model_index].predict(model_observation, done))
+
+        actions = tuple(map(lambda sar: sar[0], sample_actions_result))
+        total_action = self.vec_actions_combiner(actions)
+        # Возвращать необходимо только sar, используемые для реальных моделей, а не статических. Иначе в record
+        # из learn попадет sar от статических моделей.
+        return total_action, sample_actions_result[:models_count]
+
     def collect_step_info(self, observation, done):
 
-        left_sample_actions_result = [model.sample_action() for model in self.left_models]
-        right_sample_actions_result = [model.sample_action() for model in self.right_models]
-
-        left_actions = tuple(map(lambda sar: sar[0], left_sample_actions_result))
-        right_actions = tuple(map(lambda sar: sar[0], right_sample_actions_result))
-
-        left_total_action = self.vec_actions_combiner(left_actions)
-        right_total_action = self.vec_actions_combiner(right_actions)
+        left_total_action, left_sample_actions_result = \
+            self.get_side_total_action(self.left_models, self.left_static_models, observation, done)
+        right_total_action, right_sample_actions_result = \
+            self.get_side_total_action(self.right_models, self.right_static_models, observation, done)
 
         for env_index in range(self.env.num_envs):
             self.env.env_method("act", left_total_action[env_index], indices=env_index)
@@ -55,10 +74,11 @@ class TwoSideModelAgent(MultiModelAgent):
 
         vecenv_data = self.env.env_method("commit")
         left_observation, right_observation, _, _ = list(zip(*vecenv_data))
-        left_observation = np.array(left_observation)
-        right_observation = np.array(right_observation)
 
+        left_observation = np.array(left_observation)
         left_reward = self.env.env_method("calculate_left_reward")
+
+        right_observation = np.array(right_observation)
         right_reward = self.env.env_method("calculate_right_reward")
 
         observation = [left_observation,] * len(self.left_models) + [right_observation,] * len(self.right_models)
@@ -68,7 +88,7 @@ class TwoSideModelAgent(MultiModelAgent):
         # Используется, чтобы Monitor мог записать все необходимые логи. На самом деле данная функция не влияет на
         # состояние среды
         # TODO: Вообще, такой подход просто ужасен, но step не может сделать все сам, так как VecEnv не разрешает
-        # вернуть несколько наград.
+        # вернуть несколько наград. Возможно, стоит это делать через info?
         _, _, done, info = self.env.step(left_total_action)
 
         return observation, reward, done, info, sample_actions_result
