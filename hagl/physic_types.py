e51bfe3b5cf92eb4ba1bce54630a213ad7b1ab03
TheShenk <udod_maksim@mail.ru>
2023-04-07 16:16:02 +0300

    fix(hagl): on change coord value in vector, array should changed too

--- hagl/physic_types.py
+++ hagl/physic_types.py
@@ -9,7 +9,6 @@ from hagl.template import get_template, Template, DIMENSIONS_TEMPLATE_NAME, ANGL
 from hagl.python_types import Float
 
 COORD_NAMES = ['x', 'y', 'z', 'w']
-ARRAY_ACCESS_NAME = "array"
 
 class Vector(HAGLType):
 
@@ -23,57 +22,60 @@ class Vector(HAGLType):
         t_dimensions_count = get_template(Template(DIMENSIONS_TEMPLATE_NAME), template_values)
 
         value = cls()
-        for dimension_index in range(t_dimensions_count):
-            setattr(value, COORD_NAMES[dimension_index], gym_value[dimension_index])
-        setattr(value, ARRAY_ACCESS_NAME, np.array(gym_value[:t_dimensions_count]))
+        value.array = np.array(gym_value[:t_dimensions_count])
+
         return value
 
     @staticmethod
     def deconstruct(hagl_value, template_values):
-        return np.array(getattr(hagl_value, ARRAY_ACCESS_NAME))
+        return np.array(hagl_value.array)
 
     def __add__(self, other):
         value = Vector()
-        self_array_values = getattr(self, ARRAY_ACCESS_NAME)
 
         if isinstance(other, Vector):
-            other_array_values = getattr(other, ARRAY_ACCESS_NAME)
-            add_array_values = self_array_values+other_array_values
+            add_array_values = self.array+other.array
         elif isinstance(other, np.ndarray):
-            add_array_values = self_array_values + other
+            add_array_values = self.array + other
         elif isinstance(other, list):
-            add_array_values = self_array_values + np.array(other)
+            add_array_values = self.array + np.array(other)
         else:
             raise PhysicException(f"Unknown object to add to vector: {other}")
 
-
-        for coord_n, coord_v in zip(COORD_NAMES, add_array_values):
-            setattr(value, coord_n, coord_v)
-        setattr(value, ARRAY_ACCESS_NAME, add_array_values)
+        value.array = add_array_values
         return value
 
     def __mul__(self, other):
         value = Vector()
-        self_array_values = getattr(self, ARRAY_ACCESS_NAME)
 
         if isinstance(other, Vector):
-            other_array_values = getattr(other, ARRAY_ACCESS_NAME)
-            add_array_values = self_array_values * other_array_values
+            add_array_values = self.array * other.array
         elif isinstance(other, np.ndarray):
-            add_array_values = self_array_values * other
+            add_array_values = self.array * other
         elif isinstance(other, list):
-            add_array_values = self_array_values * np.array(other)
+            add_array_values = self.array * np.array(other)
         else:
             raise PhysicException(f"Unknown object to mul with vector: {other}")
 
-        for coord_n, coord_v in zip(COORD_NAMES, add_array_values):
-            setattr(value, coord_n, coord_v)
-        setattr(value, ARRAY_ACCESS_NAME, add_array_values)
+        value.array = add_array_values
         return value
 
+    def __setattr__(self, key, value):
+        if key in COORD_NAMES:
+            key_index = COORD_NAMES.index(key)
+            self.array[key_index] = value
+        else:
+            super().__setattr__(key, value)
+
+    def __getattr__(self, item):
+        if item in COORD_NAMES:
+            item_index = COORD_NAMES.index(item)
+            return self.array[item_index]
+        else:
+            return super().__getattribute__(item)
+
     def norm(self):
-        self_array_values = getattr(self, ARRAY_ACCESS_NAME)
-        return np.linalg.norm(self_array_values)
+        return np.linalg.norm(self.array)
 
 class Velocity(Vector):
     pass
